fb609fd3ce4b7261f7cff4ea39bba66364fdcb8b
VladZaech <rutova2@gmail.com>
2016-10-29 23:06:26 +0300

    1
    Signed-off-by: VladZaech <rutova2@gmail.com>

---
+++ app/src/main/java/com/example/webviewsample/WebViewSampleActivity.java
@@ -0,0 +1,40 @@
+package com.example.webviewsample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewSampleActivity extends Activity {
+    WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.google.com");
+        webView.setWebViewClient(new WebViewSampleClient());
+    }
+    private class WebViewSampleClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
+
